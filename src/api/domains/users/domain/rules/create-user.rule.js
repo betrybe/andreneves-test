4bb6e0b215aecf8f5b77ef2278d8b84794b858de
André Neves <andreneves3@gmail.com>
2021-09-19 20:48:25 -0300

    feat(users): basic rules for user creation

---
+++ src/api/domains/users/domain/rules/create-user.rule.js
@@ -0,0 +1,19 @@
+const {
+  validateEmail,
+} = require('../../../../../resources/validators/email.validator');
+
+function createUserRule(user) {
+  console.log('validate new user', user);
+
+  if (!user.name || !user.email || !user.password) {
+    return false;
+  }
+
+  if (!validateEmail(user.email)) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = { createUserRule };
